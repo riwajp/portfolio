5f96e91599512b5537603ba45675864082665974
Riwaj Prasai <riwajfictious@gmail.com>
2025-05-04 19:11:44 +0545

    init

---
+++ script.js
@@ -0,0 +1,64 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector(".animated-nav");
+  setTimeout(() => {
+    console.log("Adding class");
+    nav.classList.add("show");
+  }, 200); // slight delay to trigger transition
+
+  fetch("projects.json")
+    .then((response) => response.json())
+    .then((projects) => {
+      const worksList = document.querySelector(".latest-works-list");
+
+      projects.forEach((project) => {
+        const work = document.createElement("div");
+        work.classList.add("latest-work");
+        work.innerHTML = `
+        <img src="${project.image}" alt="${project.title}" />
+        <div class="latest-work-title">${project.title}</div>
+        <div class="latest-work-desc">${project.description}</div>
+      `;
+        worksList.appendChild(work);
+      });
+    })
+    .catch((error) => console.error("Failed to load projects:", error));
+
+  fetch("technologies.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.querySelector(".technologies-list");
+
+      Object.entries(data).forEach(([category, items]) => {
+        const categoryDiv = document.createElement("div");
+        categoryDiv.className = "technology-category";
+
+        const title = document.createElement("div");
+        title.className = "technology-category-title";
+        title.textContent = category;
+
+        const list = document.createElement("div");
+        list.className = "technology-category-list";
+
+        items.forEach((tech) => {
+          const techDiv = document.createElement("div");
+          techDiv.className = "technology";
+
+          const icon = document.createElement("i");
+          icon.className = tech.icon + " technology-icon";
+
+          const name = document.createElement("div");
+          name.className = "technology-name";
+          name.textContent = tech.name;
+
+          techDiv.appendChild(icon);
+          techDiv.appendChild(name);
+          list.appendChild(techDiv);
+        });
+
+        categoryDiv.appendChild(title);
+        categoryDiv.appendChild(list);
+        container.appendChild(categoryDiv);
+      });
+    })
+    .catch((err) => console.error("Failed to load technologies:", err));
+});
